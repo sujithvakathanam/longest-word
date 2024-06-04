2990c9ac66ed1e0c86387913d00164e0bc72a645
sujith vakathanam <sujithvakathanam@gmail.com>
2024-06-04 21:25:36 +0000

    Small web app wrapper around the Game

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,13 @@ from longest_word.game import Game
 app = Flask(__name__)
 # [...]
 
+@app.route('/')
+def home():
+    game = Game()
+    return render_template('home.html', grid=game.grid)
+
 @app.route('/check', methods=["POST"])
 def check():
-    return 'Hello world'
     game = Game()
     game.grid = list(request.form['grid'])
     word = request.form['word']
